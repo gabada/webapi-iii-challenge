71ff2052ae2d170b89505f68d1f91317d2d13cff
Gill Abada <gill@callifi.com>
2019-03-19 16:04:27 -0400

    install express, start basic server.

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // code away!
+const express = require('express');
+
+const port = 5000;
+const server = express();
+
+server.use(express.json());
+
+server.listen(port, () => {
+  console.log(`server listening on port ${port}`);
+});
