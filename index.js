07ebfa8558bea1786becd06534467d82948e9dc7
Gill Abada <gill@callifi.com>
2019-03-19 16:25:43 -0400

    add router for posts and get all CRUD working for posts.

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 // code away!
 const express = require('express');
 
+const postRoutes = require('./posts/postRoutes.js');
+
 const port = 5000;
 const server = express();
 
 server.use(express.json());
+server.use('/api/posts', postRoutes);
 
 server.listen(port, () => {
   console.log(`server listening on port ${port}`);
---
+++ posts/postRoutes.js
@@ -0,0 +1,99 @@
+const express = require('express');
+const db = require('../data/helpers/postDb.js');
+
+const router = express.Router();
+
+router.post('/', (req, res) => {
+  const { text, user_id } = req.body;
+  const newPost = { text, user_id };
+  if (!text || !user_id) {
+    res.status(400).json({
+      errorMessage: 'Please provide text and user_id for the post.'
+    });
+  }
+  db.insert(newPost)
+    .then(post => {
+      res.status(201).json(post);
+    })
+    .catch(err => {
+      res.status(500).json({
+        error: 'There was an error while saving the post to the database'
+      });
+    });
+});
+
+router.get('/', (req, res) => {
+  db.get()
+    .then(posts => {
+      res.status(200).json(posts);
+    })
+    .catch(err => {
+      res
+        .status(500)
+        .json({ error: 'The posts information could not be retrieved.' });
+    });
+});
+
+router.get('/:id', (req, res) => {
+  const { id } = req.params;
+  db.getById(id)
+    .then(post => {
+      if (post.id) {
+        res.status(200).json(post);
+      } else {
+        res
+          .status(404)
+          .json({ message: 'The post with the specified ID does not exist.' });
+      }
+    })
+    .catch(err => {
+      res
+        .status(500)
+        .json({ error: 'The post information could not be retrieved.' });
+    });
+});
+
+router.put('/:id', (req, res) => {
+  const { id } = req.params;
+  const { text, user_id } = req.body;
+  const updatedInfo = { text, user_id };
+  if (!text || !user_id) {
+    res.status(400).json({
+      errorMessage: 'Please provide text and user_id for the post.'
+    });
+  }
+  db.update(id, updatedInfo)
+    .then(updatePost => {
+      if (!id) {
+        res
+          .status(404)
+          .json({ message: 'The post with the specified ID does not exist.' });
+      } else {
+        db.getById(id).then(post => {
+          res.status(201).json(post);
+        });
+      }
+    })
+    .catch(err => {
+      res
+        .status(500)
+        .json({ error: 'The post information could not be modified.' });
+    });
+});
+
+router.delete('/:id', (req, res) => {
+  const { id } = req.params;
+  db.getById(id).then(post => {
+    if (post) {
+      db.remove(id).then(delId => {
+        res.status(200).json(post);
+      });
+    } else {
+      res
+        .status(400)
+        .json({ message: 'The post with the specified ID does not exist.' });
+    }
+  });
+});
+
+module.exports = router;
