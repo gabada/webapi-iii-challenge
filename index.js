0589cf7106f50238dfde4bfa2a100e9a424aaf35
Gill Abada <gill@callifi.com>
2019-03-20 13:48:32 -0400

    add helmet for security.

--- index.js
+++ index.js
@@ -11,8 +11,11 @@ const port = process.env.PORT || 4000;
 const server = express();
 
 const cors = require('cors');
+const helmet = require('helmet');
 
-server.use(express.json(), cors());
+server.use(express.json());
+server.use(cors());
+server.use(helmet());
 server.use('/api/posts', postRoutes);
 server.use('/api/users', userRoutes);
 
