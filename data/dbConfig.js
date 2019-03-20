26475306452fe003ae05207237952e593212a6c7
Gill Abada <gill@callifi.com>
2019-03-20 10:43:35 -0400

    add env variables to use prod vs dev db.

--- data/dbConfig.js
+++ data/dbConfig.js
@@ -1,4 +1,7 @@
 const knex = require('knex');
 const knexConfig = require('../knexfile.js');
 
-module.exports = knex(knexConfig.development);
+const db = process.env.DB_ENV || 'development';
+const config = require('../knexfile.js')[db];
+
+module.exports = knex(config);
--- knexfile.js
+++ knexfile.js
@@ -3,19 +3,39 @@ module.exports = {
     client: 'sqlite3',
     useNullAsDefault: true,
     connection: {
-      filename: './data/blog.db3',
+      filename: './data/blog.db3'
     },
     pool: {
       afterCreate: (conn, done) => {
         conn.run('PRAGMA foreign_keys = ON', done);
-      },
+      }
     },
     migrations: {
       directory: './data/migrations',
-      tableName: 'knex_migrations',
+      tableName: 'knex_migrations'
     },
     seeds: {
-      directory: './data/seeds',
-    },
+      directory: './data/seeds'
+    }
   },
+
+  production: {
+    client: 'sqlite3',
+    useNullAsDefault: true,
+    connection: {
+      filename: './data/prod.db3'
+    },
+    pool: {
+      afterCreate: (conn, done) => {
+        conn.run('PRAGMA foreign_keys = ON', done);
+      }
+    },
+    migrations: {
+      directory: './data/migrations',
+      tableName: 'knex_migrations'
+    },
+    seeds: {
+      directory: './data/seeds'
+    }
+  }
 };
